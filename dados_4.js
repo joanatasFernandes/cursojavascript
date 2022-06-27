ed0630b18fb87ae94edd2e6cd7e033474679ecd5
joanatasFernandes <joanatasfernandesTI@outlook.com>
2022-06-27 20:15:22 -0300

    Estrutura de controle

--- dados_4.js
+++ dados_4.js
@@ -1,6 +1,7 @@
 const a = 3;
 //....
-/*
+/* priorize criar constantes.
+let a = 10;
 a = 10;
 a = a + 10;
 */
--- desafio_area.js
+++ desafio_area.js
@@ -4,6 +4,6 @@ const PI = 3.141592
 let raio = 10;
 let areaCirc = PI * raio * raio;
 let resultado = areaCirc;
-console.log("O resultado do calculo da área é :" + resultado + "m2");
+console.log("O resultado do calculo da área é :" + resultado + " m2");
 areaCirc = Math.PI * raio * raio;
 console.log(Math.PI);
